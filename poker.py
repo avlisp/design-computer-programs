25663b7df5da9624d5d54306b5cefefd831911cc
pablocarracci <pablocarracci@gmail.com>
2020-07-13 21:57:15 -0300

    add straight and flush functions

--- poker.py
+++ poker.py
@@ -40,12 +40,33 @@ def card_ranks(cards):
     ranks.sort(reverse=True)
     return ranks
 
+def straight(ranks):
+    '''Return True if the ordered ranks form a 5-card straight.'''
+    return (max(ranks) - min(ranks) == 4) and len(set(ranks)) == 5
+
+    # My solution
+    #
+    # for i,r in enumerate(ranks):
+    #     if ranks[0] != r + i:
+    #         return False
+    # return True
+
+def flush(hand):
+    "Return True if all the cards have the same suit."
+    return len(set([s for r,s in hand])) == 1
+
 
 def test():
     '''Test cases for the functions in poker program.'''
     sf = '6C 7C 8C 9C TC'.split()
     fk = '9D 9H 9S 9C 7D'.split()
     fh = 'TD TC TH 7C 7D'.split()
+
+    assert straight([9, 8, 7, 6, 5]) == True
+    assert straight([9, 8, 8, 6, 5]) == False
+
+    assert flush(sf) == True
+    assert flush(fk) == False
 
     assert card_ranks(sf) == [10, 9, 8, 7, 6]
     assert card_ranks(fk) == [9, 9, 9, 9, 7]
