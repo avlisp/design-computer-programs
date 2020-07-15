823f13d79a85ebe5c4ccfea32d74afeebe66ecb4
pablocarracci <pablocarracci@gmail.com>
2020-07-15 15:17:24 -0300

    add two_pair()

--- poker.py
+++ poker.py
@@ -27,7 +27,6 @@ def hand_rank(hand):
 def card_ranks(cards):
     '''Return a list of the ranks, sorted with higher first.'''
     ranks = ['--23456789TJQKA'.index(r) for r,s in cards]
-
     # My solution
     # 
     # ranks = [r for r,s in cards]
@@ -43,7 +42,6 @@ def card_ranks(cards):
 def straight(ranks):
     '''Return True if the ordered ranks form a 5-card straight.'''
     return (max(ranks) - min(ranks) == 4) and len(set(ranks)) == 5
-
     # My solution
     #
     # for i,r in enumerate(ranks):
@@ -60,22 +58,38 @@ def kind(n, ranks):
     Return None if there is no n-of-a-kind in the hand.'''
     for r in ranks:
         if ranks.count(r) == n: return r
-
     # My solution
     # for i in range(14, 0, -1):
     #     if ranks.count(i) == n:
     #         return i
 
+def two_pair(ranks):
+    '''If there are two pair, return the two ranks as a
+    tuple: (highest, lowest); otherwise return None.'''
+    pair = kind(2, ranks)
+    lowpair = kind(2, list(reversed(ranks)))
+    if pair and lowpair != pair: return (pair, lowpair)
+    # My solution
+    # pairs = [r for r in ranks if ranks.count(r) == 2]
+    # pairs = list(set(pairs))
+    # if len(pairs) != 2:
+    #     return None
+    # pairs.sort(reverse=True)
+    # return tuple(pairs)
+
 
 def test():
     '''Test cases for the functions in poker program.'''
-    sf = "6C 7C 8C 9C TC".split() # Straight Flush
-    fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
-    fh = "TD TC TH 7C 7D".split() # Full House
-    tp = "5S 5D 9H 9C 6S".split() # Two pairs
+    sf = '6C 7C 8C 9C TC'.split() # Straight Flush
+    fk = '9D 9H 9S 9C 7D'.split() # Four of a Kind
+    fh = 'TD TC TH 7C 7D'.split() # Full House
+    tp = '5S 5D 9H 9C 6S'.split() # Two pairs
+
+    tpranks = card_ranks(tp)
+    assert two_pair(tpranks) == (9, 5)
+    assert two_pair([7, 4, 4, 2, 1]) == None
 
     fkranks = card_ranks(fk)
-    tpranks = card_ranks(tp)
     assert kind(4, fkranks) == 9
     assert kind(3, fkranks) == None
     assert kind(2, fkranks) == None
